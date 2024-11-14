72f761eafa7971878876794d7b88dfc3e1af3ed0
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-11-15 00:12:23 +0600

    responsive for all devices

--- src/App.jsx
+++ src/App.jsx
@@ -104,30 +104,30 @@ const handleDragEnd = (result) => {
     <div className="min-h-screen flex justify-center items-center py-6 bg-gradient-to-br from-cyan-500 to-blue-500">
       {/* body  */}
       <DragDropContext onDragEnd={handleDragEnd}>
-      <div className=" min-h-3/4 w-3/4 bg-white rounded-xl p-8">
+      <div className=" min-h-3/4 w-3/4 xl:w-3/6 bg-white rounded-xl p-8">
         {/* title and search  */}
-        <div className="flex justify-between">
-          <div className="text-3xl font-semibold">To-Do List :</div>
+        <div className="md:flex justify-between">
+          <div className="md:text-3xl text-xl font-bold">To-Do List :</div>
           <div>
             <input
               type="text"
               placeholder="Search here"
-              className="input input-bordered w-full max-w-xs"
+              className="input input-bordered w-full max-w-xs mt-2 md:mt-0"
               onChange={(e)=>setSearchText(e.target.value)}
             />
           </div>
         </div>
         {/* task add  */}
-        <div className="mt-6 ">
-          <div className="join w-full">
+        <div className="md:mt-6  mt-2">
+          <div className="md:join w-full">
             <input
-              className=" join-item outline-none border-2 border-r-0 rounded-l-full pl-3 w-full bg-gray-200"
+              className=" join-item outline-none border-2 border-r-0 md:rounded-l-full rounded-lg py-3 md:py-0 pl-3 w-full bg-gray-200"
               placeholder="Write something"
               value={taskTitle}
               onChange={(e)=>setTaskTitle(e.target.value)
               }
             />
-            <button onClick={handleAddTask} className="btn join-item rounded-r-full px-8 bg-[#ff605a] text-white hover:bg-[#f34a44]">
+            <button onClick={handleAddTask} className="btn join-item md:rounded-r-full md:px-8 bg-[#ff605a] text-white hover:bg-[#f34a44] w-full md:w-auto">
               Add
             </button>
           </div>
@@ -148,11 +148,12 @@ const handleDragEnd = (result) => {
                     <Draggable key={singleTask.id} draggableId={String(singleTask.id)} index={index}>
                       {(provided) => (
                         <li
-                          className="flex justify-between items-center p-2 bg-gray-100 rounded-lg mt-1"
+                          className="flex justify-between gap-3 items-center p-2 bg-gray-100 rounded-lg mt-1"
                           ref={provided.innerRef}
                           {...provided.draggableProps}
                           {...provided.dragHandleProps}
                         >
+                          <span className="flex w-full flex-1 md:flex-grow-0 md:w-auto">
                           <span
                             onClick={() => handleIsDone(singleTask.id)}
                             className={`material-symbols-outlined hover:cursor-pointer ${
@@ -162,7 +163,14 @@ const handleDragEnd = (result) => {
                             {singleTask.isDone ? "check_circle" : "radio_button_unchecked"}
                           </span>
                           <span
-                            className={`text-xl font-bold capitalize ${
+                            onClick={() => handleTaskDelete(singleTask.id)}
+                            className="material-symbols-outlined hover:cursor-pointer text-red-600  md:hidden"
+                          >
+                            delete
+                          </span>
+                          </span>
+                          <span
+                            className={`text-xl  md:font-bold font-semibold capitalize truncate overflow-hidden ${
                               singleTask.isDone && "line-through"
                             }`}
                           >
@@ -170,13 +178,13 @@ const handleDragEnd = (result) => {
                           </span>
                           <span
                             onClick={() => handleTaskDelete(singleTask.id)}
-                            className="material-symbols-outlined hover:cursor-pointer text-red-600"
+                            className="material-symbols-outlined hover:cursor-pointer text-red-600 hidden md:flex"
                           >
                             delete
                           </span>
                         </li>
                       )}
-                    </Draggable>
+                    </Draggable> 
                   ))}
                 {provided.placeholder}
               </ul>
