414ddc383e2eeeb95921a1dc49e25f46e327fb0e
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-11-14 11:14:33 +0600

    basic project setup and tailwind css added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <div className='h-screen flex justify-center items-center bg-gradient-to-br from-cyan-500 to-blue-500'>
+      <h1>hello</h1>
+
+    </div>
+  )
+}
+
+export default App
